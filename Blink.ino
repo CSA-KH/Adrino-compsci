f599e715d668e3aad91246bb41837ea2dcfcfbac
CSA-KH <33467712+CSA-KH@users.noreply.github.com>
2018-04-18 11:10:58 -0700

    other stuff

---
+++ Blink.ino
@@ -0,0 +1,62 @@
+const int kPinLed = 2;
+const int qPinLed = 3;
+const int wPinLed = 4;
+const int ePinLed = 5;
+const int rPinLed = 6;
+const int tPinLed = 7;
+const int yPinLed = 8;
+const int uPinLed = 9;
+const int num = 100;
+
+void setup() {
+  pinMode(kPinLed, OUTPUT);
+  pinMode(qPinLed, OUTPUT);
+  pinMode(wPinLed, OUTPUT);
+  pinMode(ePinLed, OUTPUT);
+  pinMode(rPinLed, OUTPUT);
+  pinMode(tPinLed, OUTPUT);
+  pinMode(yPinLed, OUTPUT);
+  pinMode(uPinLed, OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  delay(num);
+  digitalWrite(uPinLed, HIGH);
+  delay(num);
+  digitalWrite(kPinLed, LOW);
+  delay(num);
+  digitalWrite(qPinLed, LOW);
+  delay(num);
+  digitalWrite(kPinLed, HIGH);
+  delay(num);
+  digitalWrite(wPinLed, LOW);
+  delay(num);
+  digitalWrite(qPinLed, HIGH);
+  delay(num);
+  digitalWrite(ePinLed, LOW);
+  delay(num);
+  digitalWrite(wPinLed, HIGH);
+  delay(num);
+  digitalWrite(rPinLed, LOW);
+  delay(num);
+  digitalWrite(ePinLed, HIGH);
+  delay(num);
+  digitalWrite(tPinLed, LOW);
+  delay(num);
+  digitalWrite(rPinLed, HIGH);
+  delay(num);
+  digitalWrite(yPinLed, LOW);
+  delay(num);
+  digitalWrite(tPinLed, HIGH);
+  delay(num);
+  digitalWrite(uPinLed, LOW);
+  delay(num);
+
+  digitalWrite(yPinLed, HIGH);
+  delay(num);
+  
+  // put your main code here, to run repeatedly:
+
+}
---
+++ flip.ino
@@ -0,0 +1,39 @@
+const int rgb[] = {9, 10};
+const int time = 250;
+const int switchPin = 2;
+
+void setup() {
+  for (int i=0; i<2; i++) pinMode(rgb[i], OUTPUT);
+  pinMode(switchPin, INPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  int oldPin = 1;
+  int newPin = 0;
+  int bounce1 = digitalRead(switchPin);
+  delay(25);
+  int bounce2 = digitalRead(switchPin);
+
+  while ((bounce1 == bounce2) && (bounce1 == LOW)){
+
+
+    digitalWrite(rgb[oldPin], HIGH);
+    delay(time);
+
+    
+    if (newPin == 0) {
+      for (int i=0; i<3; i++) digitalWrite(rgb[i], HIGH);
+      delay(time);
+      for (int i=0; i<3; i++) digitalWrite(rgb[i], LOW);
+    }
+    bounce1 = digitalRead(switchPin);
+    delay(25);
+    bounce2 = digitalRead(switchPin);
+  }
+  for (int i=0; i<3; i++) digitalWrite(rgb[i], LOW);
+  digitalWrite(rgb[oldPin], LOW);
+  // put your main code here, to run repeatedly:
+
+}
